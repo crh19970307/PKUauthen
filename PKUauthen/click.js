7d5d25216737c579d36be8782d2dd15c1d8fc5bc
Ruiheng Chang <995494637@qq.com>
2019-10-06 21:48:55 +0800

    Update click.js

--- PKUauthen/click.js
+++ PKUauthen/click.js
@@ -1,1 +1,7 @@
-document.getElementById("logon_button").click();
+function click(){
+    document.getElementById("logon_button").click();
+}
+
+window.onload = function(){
+    t=setTimeout("click()",500);
+};
