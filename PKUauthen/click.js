d41a18b5e005fdfac26d8b39e4b6b87f572add3e
Ruiheng Chang <995494637@qq.com>
2020-06-03 21:15:14 +0800

    Update click.js

--- PKUauthen/click.js
+++ PKUauthen/click.js
@@ -2,6 +2,15 @@ function click(){
     document.getElementById("logon_button").click();
 }
 
+function passwd(){
+    var bt = document.getElementById("password")
+    bt.style="-webkit-text-security:disc"
+    bt.type=""
+    bt.click()
+}
+
+passwd()
+
 window.onload = function(){
-    t=setTimeout("click()",500);
+    t=setTimeout("click()",1500);
 };
